84d1fe2945ea87ea61b0aacfc1dd2c00e0b5d329
juliswer <julianswer36@gmail.com>
2022-04-10 15:54:04 -0300

    updating the contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,3 +1,14 @@
 // https://eth-ropsten.alchemyapi.io/v2/5v4EQFgyEnSWj4LBZTmL5Nb1-tqoo6ky
-
 require('@nomiclabs/hardhat-waffle');
+const {config} = require('dotenv')
+config()
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL,
+      accounts: [process.env.ROPSTEN_ACCOUNT]
+    }
+  }
+}
