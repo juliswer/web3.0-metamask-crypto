35a866aad92d903c45117274497899b216d3e2e5
juliswer <julianswer36@gmail.com>
2022-04-10 16:07:27 -0300

    some changes

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x1A675bcD7ced965F0b259C9aA6b14F4e42518697';
--- smart_contract/index.js
+++
@@ -1,4 +0,0 @@
-const {config} = require('dotenv')
-config()
-
-console.log(process.env)
